92f9f1b3e02c536e1b10f8a1e3f5c4a4092e97d9
jinal801 <jinal.desai@trootech>
2024-01-24 11:24:00 +0530

    [UPDATED] - MR:added songplaylist and songs models

--- user/models.py
+++ user/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django_extensions.db.models import TimeStampedModel, ActivatorModel, TitleDescriptionModel
 
 
 class User(AbstractUser):
@@ -12,3 +14,19 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("user:detail", kwargs={"username": self.username})
+
+
+class Songs(TitleDescriptionModel, TimeStampedModel, ActivatorModel):
+    """
+    model for the songs.
+    """
+    singer_name = models.CharField(max_length=25, null=True, blank=True)
+    profile_pic = models.ImageField(blank=True, null=True, upload_to='images/')
+
+
+class SongPlaylist(TitleDescriptionModel, TimeStampedModel, ActivatorModel):
+    """
+    model for the user's song playlist.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='users')
+    song = models.ManyToManyField(Songs)
